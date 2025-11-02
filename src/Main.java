36cdf5af793fa7344ffb7413282abf7c671286ba
aidarmukhoiarov <https://github.com/tenezma@gmail.com/helloworld.git>
2025-11-02 16:08:43 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
 
+        Product one = new Product(1, "ручка", 20, "канцтовары");
+        Product two = new Product(2, "карандаш", 15, "канцтавары");
+        Product three = new Product(3, "линейка", 12, "канцтовары");
+        Product one1 = new Product(1, "ручка", 20, "канцтовары");
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+
+
+        if (one.equals(one1)) {
+            System.out.println("объекты одинаковые");
+        } else {
+            System.out.println("объекты не одинаковые");
+        }
+
+
     }
 }
+
